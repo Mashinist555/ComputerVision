fac224c3694fefa5a4c8f465f8c998b9b0200b06
bsv5555 <bsv5555reg@yandex.ru>
2019-09-20 14:41:27 +0300

    Optimize improved bilinear interpolation

--- bayer.py
+++ bayer.py
@@ -88,35 +88,12 @@ def improved_interpolation(raw_img):
     hei = raw_img.shape[0]
     wid = raw_img.shape[1]
     res = np.zeros((hei, wid, 3), dtype=int)
-    for i in range(0, int(hei / 2) + 1):
-        for j in range(0, int(wid / 2) + 1):
-            x = i * 2
-            y = j * 2
-            for dx in range(0, 2):
-                for dy in range(0, 2):
-                    if x + dx < hei and y + dy < wid:
-                        x0 = x + dx
-                        y0 = y + dy
-                        mini = max(0, -(x0 - 2))
-                        minj = max(0, -(y0 - 2))
-                        maxi = min(5, hei - x0 + 2)
-                        maxj = min(5, wid - y0 + 2)
-                        slice = raw_img[x0 - 2 + mini:x0 - 2 + maxi, y0 - 2 + minj:y0 - 2 + maxj]
-                        if dx == 0 and dy == 1:
-                            res[x + dx, y + dy, 0] = raw_img[x + dx, y + dy]
-                        else:
-                            factor = r[dx][dy][mini:maxi, minj:maxj]
-                            res[x + dx, y + dy, 0] = int(np.sum(np.multiply(slice, factor)) / np.sum(factor))
-                        if dx + dy == 0 or dx + dy == 2:
-                            res[x + dx, y + dy, 1] = raw_img[x + dx, y + dy]
-                        else:
-                            factor = g[dx][dy][mini:maxi, minj:maxj]
-                            res[x + dx, y + dy, 1] = int(np.sum(np.multiply(slice, factor)) / np.sum(factor))
-                        if dx == 1 and dy == 0:
-                            res[x + dx, y + dy, 2] = raw_img[x + dx, y + dy]
-                        else:
-                            factor = b[dx][dy][mini:maxi, minj:maxj]
-                            res[x + dx, y + dy, 2] = int(np.sum(np.multiply(slice, factor)) / np.sum(factor))
+    mask = np.ones(raw_img.shape, dtype=int)
+    for dx in range(0, 2):
+        for dy in range(0, 2):
+            res[dx::2,dy::2,0] = np.divide(convolve2d(raw_img, r[dx,dy], mode='same'), convolve2d(mask, r[dx,dy], mode='same'))[dx::2,dy::2]
+            res[dx::2,dy::2,1] = np.divide(convolve2d(raw_img, g[dx,dy], mode='same'), convolve2d(mask, g[dx,dy], mode='same'))[dx::2,dy::2]
+            res[dx::2,dy::2,2] = np.divide(convolve2d(raw_img, b[dx,dy], mode='same'), convolve2d(mask, b[dx,dy], mode='same'))[dx::2,dy::2]
     return np.clip(res, 0, 255)
 
 
