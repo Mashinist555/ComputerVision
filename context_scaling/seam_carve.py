455afcf4e2082c976866d247c94b2330bb14fbfe
bsv5555 <bsv5555reg@yandex.ru>
2019-09-22 22:33:49 +0300

    Seam carve srink with mask

--- context_scaling/seam_carve.py
+++ context_scaling/seam_carve.py
@@ -14,21 +14,25 @@ def get_brightness(img):
     )
 
 
-def get_energy(img):
+def get_energy(img, mask):
     x_conv = np.array([[-1, 0, 1]])
     y_conv = np.array([[-1], [0], [1]])
     br_img = get_brightness(img)
     dx = convolve2d(br_img, x_conv, mode='same', boundary='symm')  # TODO boundary can be wrong
     dy = convolve2d(br_img, y_conv, mode='same', boundary='symm')  # TODO boundary can be wrong
-    return np.sqrt(np.add(
+    result = np.sqrt(np.add(
         np.power(dx, 2),
         np.power(dy, 2)
     ))
+    mult = np.size(result) * 256.0
+    if mask is not None:
+        result = np.add(result, np.multiply(mask, mult))
+    return result
 
 
-def get_line_indices(img):
-    energy = get_energy(img)
-    imsave("test_energy.png", energy, )
+def get_line_indices(img, mask):
+    energy = get_energy(img, mask)
+    # imsave("test_energy.png", energy, )
     hei = energy.shape[0]
     wid = energy.shape[1]
     dp = np.zeros(energy.shape, dtype=np.float64)
@@ -45,19 +49,34 @@ def get_line_indices(img):
 
     return result
 
-def shrink(img):
+
+def shrink(img, mask):
     hei = img.shape[0]
     wid = img.shape[1]
-    inds = get_line_indices(img)
-    result = np.zeros((hei, wid-1, 3), dtype=np.float64)
+    inds = get_line_indices(img, mask)
+    result = np.zeros((hei, wid - 1, 3), dtype=np.float64)
+    remove_mask = np.zeros((hei, wid), dtype=int)
+    res_mask = np.zeros((hei, wid - 1), dtype=int)
     for i in range(0, hei):
-        result[i] = np.concatenate((img[i,:inds[i]], img[i,inds[i]+1:]))
-    imsave("test_result.png", result, )
-    return result
+        remove_mask[i][inds[i]] = 1
+        result[i] = np.concatenate((img[i, :inds[i]], img[i, inds[i] + 1:]))
+        if mask is not None:
+            res_mask[i] = np.concatenate((mask[i, :inds[i]], mask[i, inds[i] + 1:]))
+    # imsave("test_result.png", result, )
+    if mask is not None:
+        return result, res_mask, remove_mask
+    else:
+        return result, None, remove_mask
+
 
 def seam_carve(img, mode, mask):
     (orientation, direction) = mode.split(' ')
     if orientation == 'vertical':
-        return shrink(np.transpose(img, (1,0,2)))
+        if mask is not None:
+            mask = np.transpose(mask)
+        result, res_mask, mask = shrink(np.transpose(img, (1, 0, 2)), mask)
+        # if res_mask is not None:
+        #     res_mask = np.transpose(res_mask)
+        return np.transpose(result, (1, 0, 2)), np.transpose(res_mask), np.transpose(mask)
     else:
-        return shrink(img)
+        return shrink(img, mask)
