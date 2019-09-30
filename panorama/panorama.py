8679b58a1b5c844c180072c9cf469be9d1e3a1c1
bsv5555 <bsv5555reg@yandex.ru>
2019-10-01 00:56:14 +0300

    Panorama - full implementation (gaussian pyramid merge)

--- panorama/panorama.py
+++ panorama/panorama.py
@@ -76,7 +76,7 @@ def find_homography(src_keypoints, dest_keypoints):
     y = src[:, 1]
     x1 = dest[:, 0]
     y1 = dest[:, 1]
-    for i in range(0, n):
+    for i in range(n):
         A[i * 2] = np.array([-x[i], -y[i], -1, 0, 0, 0, x1[i] * x[i], x1[i] * y[i], x1[i]])
         A[i * 2 + 1] = np.array([0, 0, 0, -x[i], -y[i], -1, y1[i] * x[i], y1[i] * y[i], y1[i]])
 
@@ -88,9 +88,8 @@ def find_homography(src_keypoints, dest_keypoints):
     return result
 
 
-def ransac_transform(src_keypoints, src_descriptors, dest_keypoints, dest_descriptors, max_trials=1000,
-                     residual_threshold=1,
-                     return_matches=False):
+def ransac_transform(src_keypoints, src_descriptors, dest_keypoints, dest_descriptors,
+                     max_trials=1000, residual_threshold=1, return_matches=False):
     """Match keypoints of 2 images and find ProjectiveTransform using RANSAC algorithm.
 
     src_keypoints ((N, 2) np.ndarray) : source coordinates
@@ -111,7 +110,7 @@ def ransac_transform(src_keypoints, src_descriptors, dest_keypoints, dest_descri
     n = matches.shape[0]
     res_inds = [-1, -1, -1, -1]
     res_kol = 0
-    for trial in range(0, max_trials):
+    for trial in range(max_trials):
         inds = random.sample(range(n), 4)
         h = find_homography(src_keypoints[matches[inds, 0]], dest_keypoints[matches[inds, 1]])
         projected = ProjectiveTransform(h)(src_keypoints[matches[:, 0]])
@@ -131,7 +130,10 @@ def ransac_transform(src_keypoints, src_descriptors, dest_keypoints, dest_descri
         np.power(projected[:, 1] - dest_keypoints[matches[:, 1], 1], 2))
     inliers = matches[dist < residual_threshold]
     transform = ProjectiveTransform(find_homography(src_keypoints[inliers[:, 0]], dest_keypoints[inliers[:, 1]]))
-    return transform, inliers
+    if return_matches:
+        return transform, inliers
+    else:
+        return transform
 
 
 def find_simple_center_warps(forward_transforms):
@@ -149,12 +151,12 @@ def find_simple_center_warps(forward_transforms):
     result[center_index] = DEFAULT_TRANSFORM()
     cur_h = DEFAULT_TRANSFORM().params
     for i in range(center_index - 1, -1, -1):
-        cur_h = np.matmul(forward_transforms[i][0].params, cur_h)
+        cur_h = np.matmul(forward_transforms[i].params, cur_h)
         result[i] = ProjectiveTransform(cur_h)
 
     cur_h = DEFAULT_TRANSFORM().params
     for i in range(center_index, image_count - 1):
-        cur_h = np.matmul(cur_h, inv(forward_transforms[i][0].params))
+        cur_h = np.matmul(cur_h, inv(forward_transforms[i].params))
         result[i + 1] = ProjectiveTransform(cur_h)
 
     return tuple(result)
@@ -218,7 +220,8 @@ def warp_image(image, transform, output_shape):
     """
     # your code here
     warped_image = warp(image, rotate_transform_matrix(transform).inverse, output_shape=output_shape)
-    mask = warp(np.ones(image.shape, dtype=np.bool8), rotate_transform_matrix(transform).inverse, output_shape=output_shape)
+    mask = warp(np.ones(image.shape, dtype=np.bool8), rotate_transform_matrix(transform).inverse,
+                output_shape=output_shape)
     return warped_image, np.asarray(mask, dtype=np.bool8)
 
 
@@ -237,13 +240,13 @@ def merge_pano(image_collection, final_center_warps, output_shape):
 
     # your code here
     n = len(image_collection)
-    for i in reversed(range(0, n)):
+    for i in reversed(range(n)):
         warped_image, mask = warp_image(image_collection[i], final_center_warps[i], output_shape)
         result[mask] = warped_image[mask]
     return result
 
 
-def get_gaussian_pyramid(image, n_layers, sigma):
+def get_gaussian_pyramid(image, n_layers, sigma=1):
     """Get Gaussian pyramid.
 
     image ((W, H, 3)  np.ndarray) : original image
@@ -255,10 +258,16 @@ def get_gaussian_pyramid(image, n_layers, sigma):
 
     """
     # your code here
-    pass
+    result = [None] * n_layers
+    img = image
+    result[0] = img
+    for i in range(1, n_layers):
+        result[i] = gaussian(img, sigma)
+        img = result[i]
+    return result
 
 
-def get_laplacian_pyramid(image, n_layers, sigma):
+def get_laplacian_pyramid(image, n_layers=5, sigma=1):
     """Get Laplacian pyramid
 
     image ((W, H, 3)  np.ndarray) : original image
@@ -269,7 +278,12 @@ def get_laplacian_pyramid(image, n_layers, sigma):
         tuple(n_layers) Laplacian pyramid
     """
     # your code here
-    pass
+    gauss = get_gaussian_pyramid(image, n_layers, sigma)
+    result = [None] * n_layers
+    for i in range(n_layers - 1):
+        result[i] = gauss[i] - gauss[i + 1]
+    result[n_layers - 1] = gauss[n_layers - 1]
+    return result
 
 
 def merge_laplacian_pyramid(laplacian_pyramid):
@@ -297,7 +311,20 @@ def increase_contrast(image_collection):
     return result
 
 
-def gaussian_merge_pano(image_collection, final_center_warps, output_shape, n_layers, image_sigma, merge_sigma):
+def blend_images(img1, img2, mask, n_layers, image_sigma, merge_sigma):
+    img1_pyr = get_laplacian_pyramid(img1, n_layers, image_sigma)
+    img2_pyr = get_laplacian_pyramid(img2, n_layers, image_sigma)
+    mask_pyr = get_gaussian_pyramid(mask, n_layers, merge_sigma)
+    result = np.zeros(img1.shape)
+    for i in range(n_layers):
+        for channel in range(3):
+            result[:,:,channel] += img1_pyr[i][:,:,channel] * np.subtract(1, mask_pyr[i])
+            result[:,:,channel] += img2_pyr[i][:,:,channel] * mask_pyr[i]
+
+    return result
+
+
+def gaussian_merge_pano(image_collection, final_center_warps, output_shape, n_layers=5, image_sigma=1, merge_sigma=1):
     """ Merge the whole panorama using Laplacian pyramid
 
     image_collection (Tuple[N]) : list of all images
@@ -311,4 +338,16 @@ def gaussian_merge_pano(image_collection, final_center_warps, output_shape, n_la
         (output_shape) np.ndarray: final pano
     """
     # your code here
-    pass
+    image_collection = increase_contrast(image_collection)
+    warps_with_masks = [warp_image(img, warp, output_shape) for img, warp in zip(image_collection, final_center_warps)]
+    corners = tuple(get_corners(image_collection, final_center_warps))
+    result = np.zeros(np.append(output_shape, 3))
+    result += warps_with_masks[0][0]
+    n = len(image_collection)
+    for i in range(1, n):
+        max_left = int((corners[i-1][2, 0] + corners[i-1][3, 0])/2)
+        min_right = int((corners[i][0, 0] + corners[i][1, 0])/2)
+        mask = np.zeros(output_shape)
+        mask[:,(max_left + min_right)//2:] = 1.0
+        result = blend_images(result, warps_with_masks[i][0], mask, n_layers, image_sigma, merge_sigma)
+    return result
