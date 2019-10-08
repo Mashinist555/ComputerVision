Sign Classification - 93-94% acc weighted 4,2,3 - fixed

sign_classification/fit_and_classify.py
@@ -9,7 +9,7 @@ import math
 import time
 
 EPS = 1e-8
-CELL_NUM = 10
+CELL_NUM = 11
 CELL_SIZE = 8
 
 current_milli_time = lambda: int(round(time.time() * 1000))
@@ -27,8 +27,8 @@ def calc_gradient(img):
     iy_res += iy[:, :, 0] * 4
     ix_res += ix[:, :, 1] * 2
     iy_res += iy[:, :, 1] * 2
-    ix_res += ix[:, :, 1] * 3
-    iy_res += iy[:, :, 1] * 3
+    ix_res += ix[:, :, 2] * 3
+    iy_res += iy[:, :, 2] * 3
     gradient_len = np.sqrt(ix_res * ix_res + iy_res * iy_res)
     gradient_dir = np.arctan2(iy_res, ix_res)
     gradient_dir[gradient_dir < 0] = gradient_dir[gradient_dir < 0] + math.pi  # TODO check
@@ -73,10 +73,10 @@ def calc_blocks(image):
     return blocks
 
 
-def extract_data():
+def extract_data(seed):
     table = pd.read_csv('public_tests/00_test_img_input/train_gt.csv')
     object_ids = table[['class_id', 'phys_id']].drop_duplicates()
-    train_ids, test_ids = train_test_split(object_ids, test_size=0.2, random_state=14)  # TODO some objects may disappear
+    train_ids, test_ids = train_test_split(object_ids, test_size=0.2, random_state=seed)  # TODO some objects may disappear
     train_id_set = set()
     for index, row in train_ids.iterrows():
         train_id_set.add((row.class_id, row.phys_id))
@@ -87,7 +87,7 @@ def extract_data():
 
 
 def fit_and_classify(train_features, train_labels, test_features):
-    model = svm.LinearSVC(verbose=1, C=0.05, max_iter=1000)
+    model = svm.LinearSVC(verbose=0, C=0.05, max_iter=1000)
     start = current_milli_time()
     len = train_features.shape[0]
     # model = svm.SVC(kernel='poly', C=0.1, verbose=1, degree=2, max_iter=1000)

sign_classification/run.py
@@ -69,7 +69,7 @@ def grade(data_path):
     return res
 
 
-def run_single_test(data_dir, output_dir):
+def run_single_test(data_dir, output_dir, seed):
     from fit_and_classify import fit_and_classify, extract_hog, extract_data
     from glob import glob
     from numpy import zeros
@@ -93,7 +93,7 @@ def run_single_test(data_dir, output_dir):
 
         return filenames, labels
 
-    HOG_FILENAME = 'train_hog_file_size80_block4_423.csv'
+    HOG_FILENAME = 'train_hog_file_size88_block4_423fixed.csv'
 
     def dump_features(path, filenames):
         hog_length = len(extract_hog(imread(join(path, filenames[0]))))
@@ -116,10 +116,10 @@ def run_single_test(data_dir, output_dir):
         for i in range(0, len(filenames)):
             filename = join(path, filenames[i])
             data[i, :] = np.asarray(hog_data.loc[filename].hog_vector.split(','), dtype=float)
-            if i % 100 == 0:
+            if i % 5000 == 0:
                 print('{} done'.format(i))
 
-        train_data, test_data = extract_data()
+        train_data, test_data = extract_data(seed)
 
         train_data['filenames'] = 'public_tests/00_test_img_input/train/' + train_data['filenames']
         test_data['filenames'] = 'public_tests/00_test_img_input/train/' + test_data['filenames']
@@ -158,10 +158,10 @@ def run_single_test(data_dir, output_dir):
         else:
             incorrect += 1
             miss[test_labels[i]]+=1
-            print("Name: {}, expected: {}, actual: {}".format(test_data.filenames[i], test_labels[i], y[i]))
+            # print("Name: {}, expected: {}, actual: {}".format(test_data.filenames[i], test_labels[i], y[i]))
 
     print(HOG_FILENAME)
-    print("Correct: {}; Incorrect: {}; Accuracy: {}".format(correct, incorrect, correct / len(y) * 100))
+    print("Correct: {}; Incorrect: {}; Accuracy: {}; Seed: {}".format(correct, incorrect, correct / len(y) * 100, seed))
     print("Per class stats:")
     for i in range(43):
         conf_matrix[i][i] //= 100
@@ -172,9 +172,9 @@ def run_single_test(data_dir, output_dir):
     sn.heatmap(df_cm, annot=True)
     plt.show()
 
-    with open(join(output_dir, 'output.csv'), 'w') as fout:
-        for i, filename in enumerate(test_filenames):
-            print('%s,%d' % (filename, y[i]), file=fout)
+    # with open(join(output_dir, 'output.csv'), 'w') as fout:
+    #     for i, filename in enumerate(test_filenames):
+    #         print('%s,%d' % (filename, y[i]), file=fout)
 
 
 if __name__ == '__main__':
@@ -223,7 +223,8 @@ if __name__ == '__main__':
 
             try:
                 start = time()
-                run_single_test(input_dir, run_output_dir)
+                for i in range(10):
+                    run_single_test(input_dir, run_output_dir, i + 100)
                 end = time()
                 running_time = end - start
             except: